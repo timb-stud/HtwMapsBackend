9e776ffdb4cca4dbeaf8907a1ca3213863deaa15
stas-bob <oot.pac@googlemail.com>
2010-06-17 22:46:23 -0700

    test an DBSchnittstelle angepasst

--- src/de/htwmaps/algorithm/tests/DijkstraStarterTest.java
+++ src/de/htwmaps/algorithm/tests/DijkstraStarterTest.java
@@ -1,32 +1,38 @@
 package de.htwmaps.algorithm.tests;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 
 import junit.framework.TestCase;
 import de.htwmaps.algorithm.DijkstraStarter;
 import de.htwmaps.algorithm.Node;
 import de.htwmaps.algorithm.PathNotFoundException;
+import de.htwmaps.database.ResultSetToArray_ohne_fehler;
 
 public class DijkstraStarterTest extends TestCase {
 
-	public void testFindShortestPath1() {
+	public void testFindShortestPath1() throws SQLException {
+		System.out.println("Dijkstra test");
+		ResultSetToArray_ohne_fehler r = new ResultSetToArray_ohne_fehler();
 		Node[] result;
 		int[] expectedResult = {1,3,5,7};
 		DijkstraStarter ds = new DijkstraStarter();
 		//
-		int[] allNodeIDs = {1,2,3,4,5,6,7};
-		float[] x = {0,6,6,16,16,22,28};
-		float[] y = {10,16,6,10,6,10,6};
-		int startNodeID = 7;
-		int goalNodeID = 1;
-		int[] fromNodeIDs = {1,1,2,3,4,4,5,6};
-		int[] toNodeIDs =   {2,3,4,5,5,6,7,7};
-		double[] fromToDistances = {8.49, 7.21, 11.66, 10, 12, 6, 4, 7.21};
-		boolean[] oneways = {false, false, false, false, false, false, false, false};
-		int[] highwayTypes = {0,0,0,0,0,0,0,0};
+		int[] allNodeIDs = r.getNodeID();
+		float[] x = r.getLon();
+		float[] y = r.getLat();
+		int startNodeID = 587836344;
+		int goalNodeID = 274026832;
+		int[] fromNodeIDs = r.getFromNodeID();
+		int[] toNodeIDs =   r.getToNodeID();
+		double[] fromToDistances = r.getLength1();
+		boolean[] oneways = r.getOneWay();
+		int[] highwayTypes = null;
 		
 		try{
+			long time = System.currentTimeMillis();
 			result = ds.findShortestPath(allNodeIDs, x, y, startNodeID, goalNodeID, fromNodeIDs, toNodeIDs, fromToDistances, oneways, highwayTypes);
+			System.out.println(System.currentTimeMillis() - time + " ms");
 			System.out.println(Arrays.toString(result));
 			for(int i = 0; i < result.length; i++){
 				assertEquals(expectedResult[i], result[i].getId());
