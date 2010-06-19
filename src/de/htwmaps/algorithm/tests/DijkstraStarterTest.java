1c71c1006f6021cae90c32f16bc91997fa6799b8
stas-bob <oot.pac@googlemail.com>
2010-06-19 22:16:50 +0200

    node->xml methode in dijkstrastartertest.java added

--- src/de/htwmaps/algorithm/tests/DijkstraStarterTest.java
+++ src/de/htwmaps/algorithm/tests/DijkstraStarterTest.java
@@ -1,42 +1,56 @@
 package de.htwmaps.algorithm.tests;
 
 
+import java.sql.SQLException;
 import java.util.Arrays;
 
+import de.htwmaps.algorithm.AStar;
 import de.htwmaps.algorithm.DijkstraStarter;
 import de.htwmaps.algorithm.Node;
 import de.htwmaps.algorithm.PathNotFoundException;
+import de.htwmaps.database.DBAdapterRectangle;
 import de.htwmaps.util.InitLogger;
 
 public class DijkstraStarterTest {
 
-	public static void main(String[] args) {
-		System.out.println("Dijkstra test");
-		Node[] result;
-		DijkstraStarter ds = new DijkstraStarter();
+	public static void main(String[] args) throws SQLException, PathNotFoundException {
 		InitLogger.INSTANCE.initLogger();
+		DijkstraStarter ds = new DijkstraStarter();
+		int startNodeID = 274026832;
+		int goalNodeID = 587836344;
+		float startNodeLon = 7.0213f;
+		float startNodeLat = 49.3431f;
+		float endNodeLon = 7.1549f;
+		float endNodeLat = 49.391f;
 		
-		//
-		int[] allNodeIDs = {1,2,3,4,5,6,7};
-		float[] x = {0,6,6,16,16,22,28};
-		float[] y = {10,16,6,10,6,10,6};
-		int startNodeID = 1;
-		int goalNodeID = 7;
-		int[] fromNodeIDs = {1,1,2,3,4,4,5,6};
-		int[] toNodeIDs =   {2,3,4,5,5,6,7,7};
-		double[] fromToDistances = {8.49, 7.21, 11.66, 10, 12, 6, 4, 7.21};
-		boolean[] oneways = {true, true, false, false, false, false, false, false};
-		int[] highwayTypes = {0,0,0,0,0,0,0,0};
-
-		try {
-			long time = System.currentTimeMillis();
-			result = ds.findShortestPath(allNodeIDs, x, y, startNodeID,
-					goalNodeID, fromNodeIDs, toNodeIDs, fromToDistances,
-					oneways, highwayTypes);
-			System.out.println(System.currentTimeMillis() - time + " ms");
-			System.out.println(Arrays.toString(result));
-		} catch (PathNotFoundException e) {
+		
+		DBAdapterRectangle dbar;
+		dbar = new DBAdapterRectangle(startNodeLon, startNodeLat, endNodeLon, endNodeLat);
+		int[] nodeIDs = dbar.getNodeIDs();
+		float[] nodeLons = dbar.getNodeLons(); //x
+		float[] nodeLats = dbar.getNodeLats(); //y
+		
+		int[] fromNodeIDs = dbar.getFromNodeIDs();
+		int[] toNodeIDs = dbar.getToNodeIDs();
+		double[] distances = dbar.getDistances();
+		boolean[] oneways = dbar.getOneways();
+		int[] highwayTypes = dbar.getHighwayTypes();
+		long time = System.currentTimeMillis();
+		Node[] result = ds.findShortestPath(nodeIDs, nodeLons, nodeLats, startNodeID, goalNodeID, fromNodeIDs, toNodeIDs, distances, oneways, highwayTypes);
+		System.out.println(System.currentTimeMillis() - time + " ms für algo");
+		System.out.println(generateXML(result));
+		System.out.println(Arrays.toString(result));
+	}
+	
+	public static String generateXML(Node[] result) {
+		StringBuilder str = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<osm version=\"0.6\" generator=\"De gute gute Generator 1.0\">\n");
+		StringBuilder way = new StringBuilder(" <way id=\"1\">\n");
+		for (Node tmp: result) {
+			str.append(" <node id=\"").append(tmp.getId()).append("\" lat=\"").append(tmp.getY()).append("\" lon=\"").append(tmp.getX()).append("\"/>\n");
+			way.append("  <nd ref=\"").append(tmp.getId()).append("\"/>\n");
 		}
+		str.append(way).append("  <tag k=\"highway\" v=\"secondary\"/>\n </way>\n <relation id=\"1\">\n  <member type=\"way\" ref=\"1\" role=\"\"/>\n  <tag k=\"route\" v=\"bicycle\"/>\n  <tag k=\"type\" v=\"route\"/>\n </relation>\n").append("</osm>");
+		return str.toString();
 	}
 
 }
