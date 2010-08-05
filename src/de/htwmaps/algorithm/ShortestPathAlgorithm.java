36373c0250057fa53aacedf86f2145de3380874b
stas-bob <oot.pac@googlemail.com>
2010-08-05 14:56:24 +0200

    speed added

--- src/de/htwmaps/algorithm/ShortestPathAlgorithm.java
+++ src/de/htwmaps/algorithm/ShortestPathAlgorithm.java
@@ -7,14 +7,14 @@ public interface ShortestPathAlgorithm {
 	public static final int MOTORWAY = 1;
 	public static final int PRIMARY = 5;
 	public static final int SECONDARY = 7;
-	public static final int ROAD = 10;
-	public static final int RESIDENTIAL = 11;
+	public static final int RESIDENTIAL = 10;
+	public static final int ROAD = 11;
 	public static final int LIVING_STREET = 13;
 	public static double MOTORWAY_SPEED = 130.0;
 	public static double PRIMARY_SPEED = 70.0;
 	public static double SECONDARY_SPEED = 50.0;
-	public static double ROAD_SPEED = 45.0;
 	public static double RESIDENTIAL_SPEED = 35.0;
+	public static double ROAD_SPEED = 45.0;
 	public static double LIVING_STREET_SPEED = 5.0;
 	
 	Node[] findShortestPath(int[] allNodeIDs,
