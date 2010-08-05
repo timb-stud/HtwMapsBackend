ddcbd240cf83fccff1ecbc089f331202de205f58
Tim Bartsch <timb.stud@gmail.com>
2010-08-05 17:12:02 +0200

    Shortestpath algorithm zu Abstrakter klasse umgebaut.

--- src/de/htwmaps/algorithm/ShortestPathAlgorithm.java
+++ src/de/htwmaps/algorithm/ShortestPathAlgorithm.java
@@ -1,32 +1,45 @@
 package de.htwmaps.algorithm;
 
 
-public interface ShortestPathAlgorithm {
-	public static final int SHORTEST_ROUTE = 1337;
-	public static final int FASTEST_ROUTE = 4711;
+public abstract class ShortestPathAlgorithm {
+	GraphData graphData;
+	
+	public static final int ROUTE_OPTION_SHORTEST = 1337;
+	public static final int ROUTE_OPTION_FASTEST = 4711;
 	public static final int MOTORWAY = 1;
 	public static final int PRIMARY = 5;
 	public static final int SECONDARY = 7;
 	public static final int RESIDENTIAL = 10;
 	public static final int ROAD = 11;
 	public static final int LIVING_STREET = 13;
-	public static int MOTORWAY_SPEED = 130;
-	public static int PRIMARY_SPEED = 70;
-	public static int SECONDARY_SPEED = 50;
-	public static int RESIDENTIAL_SPEED = 35;
-	public static int ROAD_SPEED = 45;
-	public static int LIVING_STREET_SPEED = 5;
+	public static int MOTORWAY_SPEED = 110; //Autobahn
+	public static int PRIMARY_SPEED = 80; //Landstraﬂe
+	public static int SECONDARY_SPEED = 60; //Ortsverbindung
+	public static int RESIDENTIAL_SPEED = 45; //Innerorts
+	public static int ROAD_SPEED = 50; //unclassified
+	public static int LIVING_STREET_SPEED = 5; //Spielstrasse
 	
-	Node[] findShortestPath(int[] allNodeIDs,
-							float[] lon,
-							float[] lat,
-							int startNodeID,
-							int goalNodeID,
-							int[] edgeIDs,
-							int[] edgeStartNodeIDs,
-							int[] edgeEndNodeIDs,
-							double[] edgeLengths,
-							boolean[] oneways,
-							int[] highwayTypes,
-							int searchOption) throws PathNotFoundException;
+	
+	public ShortestPathAlgorithm(GraphData gd) {
+		this.graphData = gd;
+	}
+	
+	public abstract Node[] findPath(int startNodeID, 
+									int goalNodeID, 
+									int routeOption, 
+									int motorwaySpeed, 
+									int primarySpeed,
+									int secondarySpeed,
+									int residentialSpeed,
+									int roadSpeed,
+									int livingStreetSpeed) throws PathNotFoundException;
+	
+	
+	public abstract Node[] findPath(int startNodeID, 
+									int goalNodeID, 
+									int routeOption, 
+									int motorwaySpeed, 
+									int primarySpeed,
+									int residentialSpeed) throws PathNotFoundException;
+	
 }
