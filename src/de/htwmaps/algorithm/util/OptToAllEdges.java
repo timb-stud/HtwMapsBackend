ca9136ffa5c97292b3ef46ed348f8d8f1f14393c
Tobias Lana <tobias.lana@googlemail.com>
2010-08-10 12:23:14 +0200

    fuer Volkan

--- src/de/htwmaps/algorithm/util/OptToAllEdges.java
+++ src/de/htwmaps/algorithm/util/OptToAllEdges.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 
+import de.htwmaps.algorithm.Coordinate;
 import de.htwmaps.algorithm.Edge;
 import de.htwmaps.algorithm.Node;
 import de.htwmaps.database.DBConnector;
@@ -14,8 +16,9 @@ public class OptToAllEdges {
 	String sql1 = "SELECT node1ID, node1lat, node1lon, node2ID, node2lat, node2lon, ID FROM edges_all WHERE partOfEdgesOptID = ?";
 	
 	PreparedStatement ps1;
-	Node[] completeRoute;
+	LinkedList<Coordinate> CoordList;
 	Connection con = DBConnector.getConnection();
+	Coordinate c;
 	
 	int myEdgeID;
 
@@ -41,17 +44,28 @@ public class OptToAllEdges {
 			    if((e.getSuccessor()).equals(route[i+1]) || (e.getPredecessor()).equals(route[i+1])){
 					myEdgeID = e.getID();
 			    	System.out.println(e.getID());
-
 			    }
 			}
 			ps1.setInt(1, myEdgeID);
 			System.out.println(ps1.toString());
 			rs1 = ps1.executeQuery();
 			while (rs1.next()) {
-				System.out.println("EgdeOpt: " + myEdgeID + " EdgeAll: " + rs1.getInt(7));
+				c = new Coordinate(rs1.getFloat(2), rs1.getFloat(3));
+				System.out.println(c);
+				CoordList.add(c);
+				anzahl++;
+				System.out.println("EgdeOpt: " + myEdgeID + " EdgeAll: " + rs1.getInt(7));	
+				if (rs1.isLast()) {
+					c = new Coordinate(rs1.getFloat(5), rs1.getFloat(6));
+					CoordList.add(c);
+				}
 			}
 			
 		}
+//		System.out.println("List gefuellt");
+//		for (Coordinate c : CoordList) {
+//			System.out.println(c);
+//		}
 	}
 
 }
