c57a6b74d39efc1b7ce2592300363a25fbb41890
Tobias Lana <tobias.lana@googlemail.com>
2010-08-10 13:13:40 +0200

    Ausgaben entfernt

--- src/de/htwmaps/algorithm/util/OptToAllEdges.java
+++ src/de/htwmaps/algorithm/util/OptToAllEdges.java
@@ -13,10 +13,10 @@ import de.htwmaps.database.DBConnector;
 
 public class OptToAllEdges {
 	
-	String sql1 = "SELECT node1ID, node1lat, node1lon, node2ID, node2lat, node2lon, ID FROM edges_all WHERE partOfEdgesOptID = ?";
+	String sql1 = "SELECT node1lat, node1lon, node2lat, node2lon FROM edges_all WHERE partOfEdgesOptID = ?";
 	
 	PreparedStatement ps1;
-	LinkedList<Coordinate> CoordList;
+	LinkedList<Coordinate> coordList;
 	Connection con = DBConnector.getConnection();
 	Coordinate c;
 	
@@ -26,46 +26,40 @@ public class OptToAllEdges {
 		try {
 			parseEdges(route);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// TODO Auto-generated constructor stub
 	}
 
-	private void parseEdges(Node[] route) throws SQLException {
+	private LinkedList<Coordinate> parseEdges(Node[] route) throws SQLException {
 		
-		ps1 	= con.prepareStatement(sql1);
+		ps1 		= con.prepareStatement(sql1);
+		coordList 	= new LinkedList<Coordinate>();
 
 		ResultSet rs1 = null;
-		// TODO Auto-generated method stub
 		System.out.println("First: " + route[0].getId() + " :: Last: " + route[route.length-1]);
 		for(int i=0; i <= route.length-2; i++){ 
 			for(Edge e : route[i].getEdgeList()){
 			    if((e.getSuccessor()).equals(route[i+1]) || (e.getPredecessor()).equals(route[i+1])){
 					myEdgeID = e.getID();
-			    	System.out.println(e.getID());
 			    }
 			}
 			ps1.setInt(1, myEdgeID);
-			System.out.println(ps1.toString());
 			rs1 = ps1.executeQuery();
 			while (rs1.next()) {
-				c = new Coordinate(rs1.getFloat(2), rs1.getFloat(3));
-				System.out.println(c);
-				CoordList.add(c);
-				anzahl++;
-				System.out.println("EgdeOpt: " + myEdgeID + " EdgeAll: " + rs1.getInt(7));	
+				c = new Coordinate(rs1.getFloat(1), rs1.getFloat(2));
+				coordList.add(c);
 				if (rs1.isLast()) {
-					c = new Coordinate(rs1.getFloat(5), rs1.getFloat(6));
-					CoordList.add(c);
+					c = new Coordinate(rs1.getFloat(3), rs1.getFloat(4));
+					coordList.add(c);
 				}
 			}
 			
 		}
-//		System.out.println("List gefuellt");
-//		for (Coordinate c : CoordList) {
+		System.out.println("List gefuellt");
+//		for (Coordinate c : coordList) {
 //			System.out.println(c);
 //		}
+	return coordList;
 	}
 
 }
